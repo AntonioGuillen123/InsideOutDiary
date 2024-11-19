5b02b6e3d7b5f5677e074dd074b0b97b4a7ec37a
issamchlf <issamchellaf734@gmail.com>
2024-11-19 22:09:59 +0100

    feat:add routes of apis

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\JournalController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/journal', [JournalController::class.'index'])->name('apihome');
+Route::delete('/journal/{id}',[JournalController::class,'destroy'])->name('apidestroy');
+Route::post('/journal',[JournalController::class,'store'])->name('apistore');
+Route::put('/journal/{id}',[JournalController::class,'update'])->name('apiupdate');
