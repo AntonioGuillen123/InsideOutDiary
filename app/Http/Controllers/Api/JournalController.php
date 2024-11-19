f5f110c5499fd35974881489c4a80a60ab3e2d91
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-11-19 15:57:39 +0100

    refactor: Comment out unused functions

--- app/Http/Controllers/Api/JournalController.php
+++ app/Http/Controllers/Api/JournalController.php
@@ -21,10 +21,10 @@ class JournalController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+   /*  public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
@@ -44,18 +44,18 @@ class JournalController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    /* public function show(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+   /*  public function edit(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Update the specified resource in storage.
