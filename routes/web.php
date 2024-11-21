524999999aba6e6c568bd1b248c6a8f7eb863909
Inma Gonzalez <inmagonzalez045@gmail.com>
2024-11-21 13:32:04 +0100

    merge: fix/road merge

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JournalController::class, 'index'])->name('home');
 
-Route::get('/journal{id}', [JournalController::class, 'show'])->name('showDetail');
+Route::get('/journal/{id}',[JournalController::class, 'show'])->name('/journal.show');
 
