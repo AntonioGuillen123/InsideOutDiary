f7f9f4218f441468788f7fb8a0e0fadabaad3473
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-11-21 21:09:43 +0100

    feat: Add a test for journal entries retrieval

--- tests/Feature/Api/JournalTest.php
+++ tests/Feature/Api/JournalTest.php
@@ -11,5 +11,13 @@ use App\Models\Journal;
 class JournalTest extends TestCase
 {
     use RefreshDatabase;
-    
+
+    public function test_CheckIfRecieveAllEntryOfJournalInJsonFile()
+    {
+        $journal = Journal::factory(2)->create();
+
+        $response = $this->get(route('apihome'));
+
+        $response->assertStatus(200)->assertJsonCount(2);
+    }
 }
