243119278cbaee09e01ee47ef003a9075ed3b01b
issamchlf <issamchellaf734@gmail.com>
2024-11-21 15:14:16 +0100

    fix:fix route of get

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\JournalController;
 
-Route::get('/journal', [JournalController::class.'index'])->name('apihome');
+Route::get('/journal', [JournalController::class,'index'])->name('apihome');
 Route::delete('/journal/{id}',[JournalController::class,'destroy'])->name('apidestroy');
 Route::post('/journal',[JournalController::class,'store'])->name('apistore');
 Route::put('/journal/{id}',[JournalController::class,'update'])->name('apiupdate');
