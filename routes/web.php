7fc57108e6ed20b41959a173740e74e0e76f90c6
issamchlf <issamchellaf734@gmail.com>
2024-11-21 09:06:14 +0100

    fix:fix target of journal web

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\JournalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JournalController::class, 'index'])->name('home');
-Route::get('/journal/{id}',[JournalController::class, 'show'])->name('journal.show');
+Route::get('/journal/{id}',[JournalController::class, 'show'])->name('/journal.show');
