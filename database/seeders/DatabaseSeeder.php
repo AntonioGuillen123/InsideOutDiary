93ea66bf438746a95c11404783978336222c896b
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-21 23:26:03 +0100

    feat: database finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,19 +13,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+       
         Journal::Factory()->create([
-            'entry' => 'Hoy estoy cansado',
-            'emotion' => 'img/emotions/sad.jpg'
+            'entry' => 'Hoy me siento feliz',
+            'emotion' => 'img/emotions/joy.png'
         ]);
 
         Journal::Factory()->create([
-            'entry' => 'Hoy estoy feliz',
-            'emotion' => 'img/emotions/happy.jpg'
+            'entry' => 'Hoy me siento triste',
+            'emotion' => 'img/emotions/sadness.png'
         ]);
 
         Journal::Factory()->create([
-            'entry' => 'Hoy estoy cabreado',
-            'emotion' => 'img/emotions/angry.jpg'
+            'entry' => 'Hoy estoy enfadado',
+            'emotion' => 'img/emotions/anger.png'
         ]);
-    }
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy me siento ansioso',
+            'emotion' => 'img/emotions/anxiety.png'
+        ]);
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy me siento asustado',
+            'emotion' => 'img/emotions/fear.png'
+        ]);
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy me siento nostalgico',
+            'emotion' => 'img/emotions/nostalgia.png'
+        ]);
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy estoy emocionado',
+            'emotion' => 'img/emotions/joy.png'
+        ]);
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy me siento vergonzoso',
+            'emotion' => 'img/emotions/embarrassment.png'
+        ]);
+
+        Journal::Factory()->create([
+            'entry' => 'Hoy me siento asqueado',
+            'emotion' => 'img/emotions/disgust.png'
+        ]);
+
+}
 }
