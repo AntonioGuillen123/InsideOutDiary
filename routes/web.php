430927f77813d81c7511585e37596c77dd3cae07
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-11-22 08:20:55 +0100

    refactor: Remove function

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,3 @@ use App\Http\Controllers\JournalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JournalController::class, 'index'])->name('home');
-
-Route::get('/journal/{id}',[JournalController::class, 'show'])->name('/journal.show');
-
